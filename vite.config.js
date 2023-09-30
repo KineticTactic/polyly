c534fac519a8d6f0126671a4f13cd470a787648c
KineticTactic <rudrajeetpal64@gmail.com>
2023-09-30 20:35:12 +0530

    Fix file structure

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, "src/Polyly.ts"),
-            name: "Polyly",
+            name: "polyly",
             fileName: "polyly",
         },
         rollupOptions: {
             external: [],
         },
     },
+    plugins: [dts({ entryRoot: resolve(__dirname, "src") })],
 });
